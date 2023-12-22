0dd4a62410e28eb7c0cc1ee22ba23c9115171785
RDaniil <danrumyantsev@yandex.ru>
2023-12-22 21:59:29 +0500

    #13 Примитивное чтение из REX файла

--- game/src/main/java/com/vdn/lampbearer/config/GameConfig.java
+++ game/src/main/java/com/vdn/lampbearer/config/GameConfig.java
@@ -7,7 +7,7 @@ import org.hexworks.zircon.api.component.ColorTheme;
 
 public class GameConfig {
 
-    public static final ColorTheme THEME = ColorThemes.zenburnVanilla();
+    public static final ColorTheme THEME = ColorThemes.hexworks();
     public static final int WINDOW_WIDTH = 80;
     public static final int WINDOW_HEIGHT = 50;
     public static final int SIDEBAR_WIDTH = 20;
@@ -16,7 +16,7 @@ public class GameConfig {
 
     public static AppConfig getAppConfig() {
         return AppConfig.newBuilder()
-                .withDefaultTileset(CP437TilesetResources.raving16x16())
+                .withDefaultTileset(CP437TilesetResources.anikki16x16())
                 .withSize(WINDOW_WIDTH, WINDOW_HEIGHT)
                 .build();
     }
